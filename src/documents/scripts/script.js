fb3fad75cc161147f2d98da862890d821f48c679
Steve McArthur <stephenmcur@gmail.com>
2014-09-17 20:45:24 +0100

    added scroll code to fix navbar

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -1,1 +1,20 @@
-/* Your scripts go here */
+/** Global window, document **/
+
+  
+
+ function scrollFn(){
+        var scrollTop = window.screenY;
+	    var headerHeight =  document.getElementById('header').offsetHeight;
+	    var navEl = document.getElementById('nav');
+	 	console.log('scroll top : ' + scrollTop);
+        if(scrollTop >= headerHeight)  {
+            navEl.className += 'fixed';
+        } 
+
+        if(scrollTop < headerHeight)
+        {
+              navEl.className = navEl.className.replace( /(?:^|\s)fixed(?!\S)/g , '' );
+        }
+    }
+
+document.onscroll = scrollFn;
